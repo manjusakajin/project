64fbd7183db7b94eb7bc4b7462a6990529dc2b1b
manjusaka <sakurano96@gmail.com>
2017-12-19 22:45:04 +0700

    thêm hướng dẫn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Admin",
+             email: "admin@gmail.com",
+             password:              "admin",
+             password_confirmation: "admin",
              activated: true,
              admin: true)
 
